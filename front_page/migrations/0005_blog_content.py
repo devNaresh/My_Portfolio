630ce01e4a11f662eae8334a3c6a9e1d65378dc1
Naresh <naresh.k@credr.com>
2015-11-04 00:16:29 +0530

    removed education and add projects

--- front_page/migrations/0005_blog_content.py
+++ front_page/migrations/0005_blog_content.py
@@ -2,19 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import tinymce.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('front_page', '0004_blog_explanation'),
-    ]
-
-    operations = [
-        migrations.AddField(
-            model_name='blog',
-            name='content',
-            field=tinymce.models.HTMLField(blank=True),
-        ),
     ]
